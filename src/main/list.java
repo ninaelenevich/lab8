15e7ca2c865fe1d743e6457e69a4e40f6b5d833c
ninaelenevich <elenevich.nina@gmail.com>
2022-12-15 15:35:33 +0300

    commen54

--- src/main/list.java
+++ src/main/list.java
@@ -5,7 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.*; //llkk
+import java.util.*; //lkjtjhgf
 
 public class list {
     public static void main(String[] args) throws FileNotFoundException {
